HardenedBSD/hardenedBSD-playground

PAX MPROTECT: added initial glues
[...]
20:31 <_op_> pax mprotect-nel PROT_MAY{READ,WRITE,EXEC} mennyire tori a mar meglevo implementaciokat?
20:31 <_op_> Az megvan, hogy a nem letezo flagekre az mmap hibat ad vissza, de gyakorlatban ez mennyi alkalmazas kb?
20:32 <pipacs> a MAY* flageket a kernel tartja nyilvan, userland nem befolyasolja
20:32 <pipacs> kb mint a MAXPROT bsd alatt asszem
[...]
20:33 <pipacs> az mprotect elvesz par MAY* flaget a W^X megvalositasa erdekeben (mmap idoben), amitol kesobbi mprotect-ek mar nem engednek W-X atmenetet egyik iranyban sem
20:33 <pipacs> minden program elhasal ezen ami futasidoben akarna kodot generalni
20:33 <_op_> igen, maxprotta kell atforditani bsd-k alatt, csak a _MAY*-ot latom celszerunek kivezetni userland-be, ha mar te ugy csinaltad, es legyen mar ilyen szinten kompatibilis a ketto
20:33 <pipacs> mondom, a MAY* flageket nem latja/befolyasolja a userland
20:34 <pipacs> bar anno volt ra otlet hogy jo lenne ha tudna kontrollalni egy app de ehhez kozossegi konszenzus kene hiszen posix api-rol van szo
20:34 <pipacs> azota se lett belole semmi :)
20:34 <_op_> akkor a draftban meg ez van benne
20:35 <pipacs> ehelyett paxban a kernel probal okoskodni pl. TEXTREL meg RELRO ugyben
20:35 <pipacs> ja hat az a doksi azert eleg regi mar, valtoztak azota a dolgok
20:36 <_op_> az alapjan kezdtem el indulni es kb megvan, hogy hogy lehet attolni a rendszeren az uj flageket, csak gondoltam elotte rakerdezek, hogy mennyire esszeru
20:36 <pipacs> en azt mondom hogy ha MPROTECT-et akartok csinalni, akkor csak ovatosan, mert sokan fognak rinyalmi miatta ;)
[...]
20:36 <_op_> nincs az a vilag, hogy az attoljuk rajtuk
20:36 <pipacs> desktopon az osszes bongeszo a jit enginek miatt, java, ffi, stb
20:37 <pipacs> szerveren java/v8 jatszik leginkabb
20:37 <_op_> akkor emutramp-ot is meg kell csinalni ffi miatt
20:37 <pipacs> ja meg az opengl csoda libek ;)
20:38 <_op_> multkor lattam valamit kint twitteren toletek, hogy az nvidia alkotott valamit

sys/hardenedbsd/hbsd_pax_common.c
@@ -110,10 +110,10 @@ pax_get_flags(struct proc *p, uint32_t *flags)
 int
 pax_elf(struct image_params *imgp, uint32_t mode)
 {
-	u_int flags, flags_aslr, flags_segvuard, flags_hardening;
+	u_int flags, flags_aslr, flags_segvuard, flags_hardening, flags_mprotect;
 
 	flags = mode;
-	flags_aslr = flags_segvuard = flags_hardening = 0;
+	flags_aslr = flags_segvuard = flags_hardening = flags_mprotect = 0;
 
 	if ((flags & ~PAX_NOTE_ALL) != 0) {
 		pax_log_aslr(imgp->proc, __func__, "unknown paxflags: %x\n", flags);
@@ -136,6 +136,10 @@ pax_elf(struct image_params *imgp, uint32_t mode)
 	flags_aslr = pax_aslr_setup_flags(imgp, mode);
 #endif
 
+#ifdef PAX_MPROTECT
+	flags_mprotect = pax_mprotect_setup_flags(imgp, mode);
+#endif
+
 #ifdef PAX_SEGVGUARD
 	flags_segvuard = pax_segvguard_setup_flags(imgp, mode);
 #endif
@@ -144,7 +148,7 @@ pax_elf(struct image_params *imgp, uint32_t mode)
 	flags_segvuard = pax_hardening_setup_flags(imgp, mode);
 #endif
 
-	flags = flags_aslr | flags_segvuard | flags_hardening;
+	flags = flags_aslr | flags_mprotect | flags_segvuard | flags_hardening;
 
 	CTR3(KTR_PAX, "%s : flags = %x mode = %x",
 	    __func__, flags, mode);
@@ -176,6 +180,7 @@ pax_init_prison(struct prison *pr)
 
 	pax_aslr_init_prison(pr);
 	pax_hardening_init_prison(pr);
+	pax_mprotect_init_prison(pr);
 	pax_segvguard_init_prison(pr);
 	pax_ptrace_hardening_init_prison(pr);
 

NexBSD/NextBSD

sys/hardenedbsd/hbsd_pax_common.c
@@ -187,10 +187,10 @@ pax_check_conflicting_modes(uint32_t mode)
 int
 pax_elf(struct image_params *imgp, uint32_t mode)
 {
-	uint32_t flags, flags_aslr, flags_segvuard, flags_hardening;
+	uint32_t flags, flags_aslr, flags_segvuard, flags_hardening, flags_mprotect;
 
 	flags = mode;
-	flags_aslr = flags_segvuard = flags_hardening = 0;
+	flags_aslr = flags_segvuard = flags_hardening = flags_mprotect = 0;
 
 	if (pax_validate_flags(flags) != 0) {
 		pax_log_internal(imgp->proc, PAX_LOG_DEFAULT,
@@ -215,6 +215,10 @@ pax_elf(struct image_params *imgp, uint32_t mode)
 	flags_aslr = pax_aslr_setup_flags(imgp, mode);
 #endif
 
+#ifdef PAX_MPROTECT
+	flags_mprotect = pax_mprotect_setup_flags(imgp, mode);
+#endif
+
 #ifdef PAX_SEGVGUARD
 	flags_segvuard = pax_segvguard_setup_flags(imgp, mode);
 #endif
@@ -223,7 +227,7 @@ pax_elf(struct image_params *imgp, uint32_t mode)
 	flags_hardening = pax_hardening_setup_flags(imgp, mode);
 #endif
 
-	flags = flags_aslr | flags_segvuard | flags_hardening;
+	flags = flags_aslr | flags_mprotect | flags_segvuard | flags_hardening;
 
 	CTR3(KTR_PAX, "%s : flags = %x mode = %x",
 	    __func__, flags, mode);
@@ -266,6 +270,7 @@ pax_init_prison(struct prison *pr)
 
 	pax_aslr_init_prison(pr);
 	pax_hardening_init_prison(pr);
+	pax_mprotect_init_prison(pr);
 	pax_segvguard_init_prison(pr);
 	pax_ptrace_hardening_init_prison(pr);
 