b35e04b6df38b259a44f86c227a5395e925d03a5
Scott K <scott@ixsystems.com>
2016-02-07 22:06:44 +0100

    integrate BFS Scheduler

--- sys/kern/kern_switch.c
+++ sys/kern/kern_switch.c
@@ -340,7 +340,21 @@ runq_add(struct runq *rq, struct thread *td, int flags)
 	struct rqhead *rqh;
 	int pri;
 
+#ifdef SCHED_FBFS
+        if (td->td_priority >= PRI_MIN_IDLE) {					// >=224
+                pri = RQ_IDLE;							// 63
+        } else if (td->td_priority >= PRI_MIN_TIMESHARE) {			// >=120
+                pri = RQ_TIMESHARE;						// 62
+        } else if (td->td_priority >= PRI_MIN_REALTIME) {			// >=48
+                pri = imin(RQ_MIN_REALTIME + td->td_priority - PRI_MIN_REALTIME,	// 61-12
+                RQ_MAX_REALTIME);
+        } else {								// < 48
+                pri = td->td_priority / RQ_PPQ;					// 11-0
+        }
+#else
 	pri = td->td_priority / RQ_PPQ;
+#endif
+
 	td->td_rqindex = pri;
 	runq_setbit(rq, pri);
 	rqh = &rq->rq_queues[pri];
--- sys/sys/runq.h
+++ sys/sys/runq.h
@@ -40,6 +40,13 @@ struct thread;
 #define	RQ_NQS		(64)		/* Number of run queues. */
 #define	RQ_PPQ		(4)		/* Priorities per queue. */
 
+#ifdef SCHED_FBFS
+#define	RQ_IDLE		(RQ_NQS - 1)
+#define	RQ_TIMESHARE	(RQ_IDLE - 1)
+#define	RQ_MIN_REALTIME	(PRI_MIN_REALTIME / 4)
+#define	RQ_MAX_REALTIME	(RQ_TIMESHARE - 1)
+#endif
+
 /*
  * Head of run queues.
  */
