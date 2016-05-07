7f41f2a8d72ba45877e65e666e22ed29aab06f82
Matt Macy <mmacy@nextbsd.org>
2016-05-06 17:42:48 -0700

    revert local changes nexus changes

--- sys/x86/x86/nexus.c
+++ sys/x86/x86/nexus.c
@@ -127,7 +127,6 @@ static	int nexus_set_resource(device_t, device_t, int, int,
 static	int nexus_get_resource(device_t, device_t, int, int,
 			       rman_res_t *, rman_res_t *);
 static void nexus_delete_resource(device_t, device_t, int, int);
-static	int nexus_get_cpus(device_t, device_t, enum cpu_sets, cpuset_t *, int size);
 #ifdef DEV_APIC
 static	int nexus_alloc_msi(device_t pcib, device_t dev, int count, int maxcount, int *irqs);
 static	int nexus_release_msi(device_t pcib, device_t dev, int count, int *irqs);
@@ -164,7 +163,6 @@ static device_method_t nexus_methods[] = {
 	DEVMETHOD(bus_set_resource,	nexus_set_resource),
 	DEVMETHOD(bus_get_resource,	nexus_get_resource),
 	DEVMETHOD(bus_delete_resource,	nexus_delete_resource),
-	DEVMETHOD(bus_get_cpus,		nexus_get_cpus),
 
 	/* pcib interface */
 #ifdef DEV_APIC
@@ -619,21 +617,6 @@ nexus_delete_resource(device_t dev, device_t child, int type, int rid)
 	struct resource_list	*rl = &ndev->nx_resources;
 
 	resource_list_delete(rl, type, rid);
-}
-
-static int
-nexus_get_cpus(device_t dev, device_t child, enum cpu_sets op, cpuset_t *cpuset, int size)
-{
-
-	switch (op) {
-#ifdef SMP
-	case INTR_CPUS:
-		*cpuset = intr_cpus;
-		return (0);
-#endif
-	default:
-		return (bus_generic_get_cpus(dev, child, op, cpuset, size));
-	}
 }
 
 /* Called from the MSI code to add new IRQs to the IRQ rman. */
